d70f49a8a3dc65428632fda90bce06c6025d58e4
Shubham Shewale <99116184+shubham-0505@users.noreply.github.com>
2023-03-22 22:30:51 +0530

    Add files via upload

---
+++ Pattern5.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cout<<"Enter the number of rows : ";
+    cin>>n;
+
+    int row = 1;
+
+    while(row<=n){
+
+        int column = 1;
+
+        while(column<=row){
+            cout<<row;
+            column = column + 1;
+        }
+        cout<<endl;
+        row = row + 1;
+    }
+    return 0;
+}
